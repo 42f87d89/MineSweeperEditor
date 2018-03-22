242abd8e9a7703b8e1077753c67c886a5b9bc0e2
Gustavo Smidth Buschle <gbuschle@gmail.com>
2018-03-22 19:15:15 +0100

    implemented serialization

--- src/Minefield.kt
+++ src/Minefield.kt
@@ -1,3 +1,5 @@
+import kotlin.js.Math.random
+
 enum class SpotState {
     Hidden,
     Shown,
@@ -18,12 +20,15 @@ fun Minefield.toIterator(): Iterator<Spot> {
         var x = 0
         var y = 0
         override fun next(): Spot {
-            if (x < a.width - 1) x += 1
+            val result = a[y][x]
+            if (x < a.width - 1) {
+                x += 1
+            }
             else {
                 x = 0
                 y += 1
             }
-            return a[y][x]
+            return result
         }
 
         override fun hasNext(): Boolean {
@@ -37,10 +42,10 @@ fun Minefield.around(x: Int, y: Int): Iterator<Pair<Int, Int>> {
     val height = this.height
     val width = this.width
     val iter = object : Iterator<Pair<Int, Int>> {
-        val maxI = if(x+1<width) 1 else 0
-        val maxJ = if(y+1<height) 1 else 0
-        val minI = if(x-1>=0) -1 else 0
-        val minJ = if(y-1>=0) -1 else 0
+        val maxI = if (x + 1 < width) 1 else 0
+        val maxJ = if (y + 1 < height) 1 else 0
+        val minI = if (x - 1 >= 0) -1 else 0
+        val minJ = if (y - 1 >= 0) -1 else 0
         //future position
         var i = minI
         var j = minJ
@@ -50,7 +55,7 @@ fun Minefield.around(x: Int, y: Int): Iterator<Pair<Int, Int>> {
         }
 
         override fun next(): Pair<Int, Int> {
-            val next = Pair(x+i, y+j)
+            val next = Pair(x + i, y + j)
             squareNext()
             if (i == 0 && j == 0) squareNext()
             return next
@@ -79,7 +84,7 @@ fun Minefield.getMines(x: Int, y: Int): Int {
 fun Minefield.getFlags(x: Int, y: Int): Int {
     var result = 0
     for ((i, j) in this.around(x, y)) {
-            if (this[j][i].state == SpotState.Flagged) result += 1
+        if (this[j][i].state == SpotState.Flagged) result += 1
     }
     return result
 }
@@ -97,52 +102,130 @@ fun serialize(f: Minefield): String {
     fun numerify(spot: Spot): Int {
         var result = 0
         if (spot.mine) result += 1
-        when {
-            spot.state == SpotState.Hidden -> result += 2
-            spot.state == SpotState.Flagged -> result += 4
+        result += when (spot.state) {
+            SpotState.Shown -> SpotState.Shown.ordinal*2
+            SpotState.Hidden -> SpotState.Hidden.ordinal*2
+            SpotState.Flagged -> SpotState.Flagged.ordinal*2
         }
         return result
     }
 
     fun base36(n: Int): Char {
-        when {
-            n < 10 -> return n.toString()[0]
-            n == 10 -> return 'a'
-            n == 11 -> return 'b'
-            n == 12 -> return 'c'
-            n == 13 -> return 'd'
-            n == 14 -> return 'e'
-            n == 15 -> return 'f'
-            n == 16 -> return 'g'
-            n == 17 -> return 'h'
-            n == 18 -> return 'i'
-            n == 19 -> return 'j'
-            n == 20 -> return 'k'
-            n == 21 -> return 'l'
-            n == 22 -> return 'm'
-            n == 23 -> return 'n'
-            n == 24 -> return 'o'
-            n == 25 -> return 'p'
-            n == 26 -> return 'q'
-            n == 27 -> return 'r'
-            n == 28 -> return 's'
-            n == 29 -> return 't'
-            n == 30 -> return 'u'
-            n == 31 -> return 'v'
-            n == 32 -> return 'w'
-            n == 33 -> return 'x'
-            n == 34 -> return 'y'
-            else -> return 'z'
+        when (n) {
+            10 -> return 'a'
+            11 -> return 'b'
+            12 -> return 'c'
+            13 -> return 'd'
+            14 -> return 'e'
+            15 -> return 'f'
+            16 -> return 'g'
+            17 -> return 'h'
+            18 -> return 'i'
+            19 -> return 'j'
+            20 -> return 'k'
+            21 -> return 'l'
+            22 -> return 'm'
+            23 -> return 'n'
+            24 -> return 'o'
+            25 -> return 'p'
+            26 -> return 'q'
+            27 -> return 'r'
+            28 -> return 's'
+            29 -> return 't'
+            30 -> return 'u'
+            31 -> return 'v'
+            32 -> return 'w'
+            33 -> return 'x'
+            34 -> return 'y'
+            35 -> return 'z'
+            else -> return n.toString()[0]
         }
     }
 
     var result = "${f.width} "
     var iter = f.toIterator()
+    var i = 0
     for (s in iter) {
-        var asd = numerify(s)
-        if (iter.hasNext()) asd += 6 * numerify(iter.next())
-        result += base36(asd)
+        var num = numerify(s)
+        if (iter.hasNext() && i<f.width) num += 6 * numerify(iter.next())
+        else i = 0
+        result += base36(num)
     }
+    return result
+}
+
+fun deserialize(data: String): Minefield {
+    fun denumerify(n: Int): Spot {
+        val statenum = n/2
+        return Spot(
+                when (statenum) {
+                    SpotState.Shown.ordinal -> SpotState.Shown
+                    SpotState.Hidden.ordinal -> SpotState.Hidden
+                    else -> SpotState.Flagged
+                }
+                , n % 2 == 1)
+    }
+
+    fun base36(n: Char): Int {
+        when (n) {
+            'a' -> return 10
+            'b' -> return 11
+            'c' -> return 12
+            'd' -> return 13
+            'e' -> return 14
+            'f' -> return 15
+            'g' -> return 16
+            'h' -> return 17
+            'i' -> return 18
+            'j' -> return 19
+            'k' -> return 20
+            'l' -> return 21
+            'm' -> return 22
+            'n' -> return 23
+            'o' -> return 24
+            'p' -> return 25
+            'q' -> return 26
+            'r' -> return 27
+            's' -> return 28
+            't' -> return 29
+            'u' -> return 30
+            'v' -> return 31
+            'w' -> return 32
+            'x' -> return 33
+            'y' -> return 34
+            'z' -> return 35
+            else -> return n.toString().toInt()
+        }
+    }
+    val asd = data.split(' ')
+    val field = asd[1]
+    val width = asd[0].toInt()
+    val height = field.length/width*2
+    val result = Minefield(width, height)
+
+    var i = 0
+    var j = 0
+    for (c in field) {
+        val num = base36(c)
+        val spot1 = denumerify(num%6)
+        result[j][i].state = spot1.state
+        result[j][i].mine = spot1.mine
+        i += 1
+        if (i > width-1) {
+            i = 0
+            j += 1
+            continue
+        }
+        val spot2 = denumerify((num - num%6)/6)
+        result[j][i].state = spot2.state
+        result[j][i].mine = spot2.mine
+        i += 1
+        if (i > width-1) {
+            i = 0
+            j += 1
+        }
+    }
+
     return result
 }
 
@@ -162,4 +245,12 @@ fun fromASCII(f: String): Minefield {
         }
     }
     return Minefield(width, height, field)
+}
+
+fun makeRandom(): Minefield {
+    val f = Minefield(20, 20)
+    for (s in f.toIterator()) {
+        s.mine = random()<0.2
+    }
+    return f
 }
