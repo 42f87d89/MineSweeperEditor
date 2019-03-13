7385ec17a45e3b706630130b7e5f85a21fae6325
Gustavo <gbuschle@hotmail.com>
2019-03-13 02:10:38 +0100

    flagged doubleclick unhides

--- src/PlayBehaviour.kt
+++ src/PlayBehaviour.kt
@@ -11,6 +11,7 @@ open class PlayBehaviour: Behaviour() {
     }
 
     override fun doubleClicked(field: Minefield, x: Int, y: Int): List<Pair<Int, Int>> {
+        field[y][x].state = SpotState.Shown //This only matters when the second click leaves a flagged spot, as unhide won't work then
         return field.unhide(x, y)
     }
 }
